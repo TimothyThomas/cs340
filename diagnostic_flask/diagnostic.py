2e4944189913d91bea9171fab6fe3fa38feed3ec
Tim Thomas <thomasti@oregonstate.edu>
2017-09-30 11:35:43 -0400

    changed DB connection to autocommit=True

--- diagnostic_flask/diagnostic.py
+++ diagnostic_flask/diagnostic.py
@@ -3,21 +3,27 @@ import pymysql.cursors
 
 app = Flask(__name__)
 
+connection = pymysql.connect(host='classmysql.engr.oregonstate.edu',
+                            user='cs340_thomasti',
+                            password='9379',
+                            db='cs340_thomasti',
+                            autocommit=True,
+                            cursorclass=pymysql.cursors.DictCursor)
+
+
 @app.route('/')
 def demo_page():
-    connection = pymysql.connect(host='classmysql.engr.oregonstate.edu',
-                                user='cs340_thomasti',
-                                password='9379',
-                                db='cs340_thomasti',
-                                cursorclass=pymysql.cursors.DictCursor)
+    try: 
+        with connection.cursor() as cursor:
+            create_string = "CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT,text VARCHAR(255) NOT NULL)"
+            cursor.execute('DROP TABLE IF EXISTS diagnostic')
+            cursor.execute(create_string)
+            cursor.execute('INSERT INTO diagnostic (`text`) VALUES ("MySQL is Working!")')
+            cursor.execute('SELECT * FROM diagnostic')
+            results = cursor.fetchall()
+    except:
+        results = {'error': 'PyMySQL error, check logs'}
 
-    cursor = connection.cursor()
-    create_string = "CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT,text VARCHAR(255) NOT NULL)"
-    cursor.execute('DROP TABLE IF EXISTS diagnostic')
-    cursor.execute(create_string)
-    cursor.execute('INSERT INTO diagnostic (`text`) VALUES ("MySQL is Working!")')
-    cursor.execute('SELECT * FROM diagnostic')
-    results = cursor.fetchall()
     return render_template('index.html', results=results)
 
 if __name__ == '__main__':
