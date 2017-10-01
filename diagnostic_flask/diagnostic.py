75b8b4c601832a76801ae156e734e5048e62b1b9
Tim Thomas <thomasti@oregonstate.edu>
2017-10-01 10:50:35 -0400

    moved DB connection inside route

--- diagnostic_flask/diagnostic.py
+++ diagnostic_flask/diagnostic.py
@@ -3,32 +3,30 @@ import pymysql.cursors
 
 app = Flask(__name__)
 
-connection = pymysql.connect(host='classmysql.engr.oregonstate.edu',
-                            user='cs340_thomasti',
-                            password='9379',
-                            db='cs340_thomasti',
-                            autocommit=True,
-                            cursorclass=pymysql.cursors.DictCursor)
-
 
 @app.route('/')
 def demo_page():
-    #try: 
+    connection = pymysql.connect(host='classmysql.engr.oregonstate.edu',
+                                user='cs340_thomasti',
+                                password='9379',
+                                db='cs340_thomasti',
+                                autocommit=True,
+                                cursorclass=pymysql.cursors.DictCursor)
+
     with connection.cursor() as cursor:
         create_string = "CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT,text VARCHAR(255) NOT NULL)"
-        try:
-            cursor.execute('DROP TABLE IF EXISTS diagnostic')
-        except:
-            pass
+        cursor.execute('DROP TABLE IF EXISTS diagnostic')
         cursor.execute(create_string)
         cursor.execute('INSERT INTO diagnostic (`text`) VALUES ("MySQL is Working!")')
         cursor.execute('SELECT * FROM diagnostic')
         results = cursor.fetchall()
-    #except:
-    #    results = {'error': 'PyMySQL error, check logs'}
 
     return render_template('index.html', results=results)
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=19538)
-    app.run()
+    app.run(host='0.0.0.0', port=19538, 
+            connect_timeout=60,
+            read_timeout=60,
+            write_timeout=60,
+            )
+    app.run(debug=True)
