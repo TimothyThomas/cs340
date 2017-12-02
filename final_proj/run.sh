4d8556bed6d90ac6024c1990464143ab3940d5e3
Tim Thomas <thomasti@oregonstate.edu>
2017-12-02 15:46:53 -0500

    Updated run script with path to python interpreter

--- final_proj/run.sh
+++ final_proj/run.sh
@@ -1,5 +1,7 @@
-source activate cs340
+PYTHON=~/miniconda3/envs/cs340/bin/python
+#PYTHON=/nfs/stak/users/thomasti/miniconda3/envs/cs340/bin/python
 export FLASK_APP=cryptocount/cryptocount.py
+echo "running app on $HOSTNAME"
 flask initdb
-./node_modules/forever/bin/forever start flask run 
+./node_modules/forever/bin/forever start -c $PYTHON cryptocount/cryptocount.py 
 ./node_modules/forever/bin/forever list
