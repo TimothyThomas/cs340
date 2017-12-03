e29e6bec0ac8d6452df1dfdd9419421915c13694
Tim Thomas <thomasti@oregonstate.edu>
2017-12-03 13:05:56 -0500

    Updated run.sh

--- final_proj/run.sh
+++ final_proj/run.sh
@@ -1,8 +1,8 @@
-PYTHON=/nfs/stak/users/thomasti/miniconda3/envs/cs340/bin/python
-#PYTHON=/home/tim/anaconda3/envs/cs340/bin/python
+PYTHON_ENV=/nfs/stak/users/thomasti/miniconda3/envs/cs340/bin/
+#PYTHON_ENV=/home/tim/anaconda3/envs/cs340/bin/
 PORT=$1
 export FLASK_APP=cryptocount/cryptocount.py
 echo "running app on $HOSTNAME and port $PORT"
-flask initdb
-./node_modules/forever/bin/forever start -c $PYTHON cryptocount/cryptocount.py $PORT
+$PYTHON_ENV/flask initdb
+./node_modules/forever/bin/forever start -c $PYTHON_ENV/python cryptocount/cryptocount.py $PORT
 ./node_modules/forever/bin/forever list
